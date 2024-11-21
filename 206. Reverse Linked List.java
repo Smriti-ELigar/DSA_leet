9bf49a4518d9360dea4b88bc7235f6fcec6947d9
Smriti Eligar <smritieligar1005@gmail.com>
2024-11-21 05:41:38 +0530

    Update 206. Reverse Linked List.java

--- 206. Reverse Linked List.java
+++ 206. Reverse Linked List.java
@@ -12,7 +12,7 @@ class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode curr = head;
         ListNode prev = null;
-        while(temp!=null){
+        while(curr!=null){
             ListNode front = curr.next;
             curr.next=prev;
             prev=curr;
@@ -22,3 +22,5 @@ class Solution {
         return prev;   
     }
 }
+// 1->2->3
+// u want 2 to point 1 instead of 3, 3 to 2 and 1 to 3
