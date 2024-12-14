77c20b4b6b4e16e27baec0d2e04375e1175c75e9
Smriti Eligar <smritieligar1005@gmail.com>
2024-12-14 14:44:47 +0530

    Time: 4 ms (89.19%), Space: 44.5 MB (71.07%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,19 +1,34 @@
+import java.util.HashMap;
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if(s.length() == 0) return 0;
+        HashMap<Character, Integer> mpp = new HashMap<>();
         int l = 0;
-        Set < Character > set = new HashSet < > ();
-        int maxans = 0;
-        for(int r = 0; r< s.length(); r++){
-            if(set.contains(s.charAt(r))){
-                while(l<r && set.contains(s.charAt(r))){
-                    set.remove(s.charAt(l));
-                    l++;
-                }
+        int r = 0;
+        int maxlen = 0;
+        int n = s.length();
+
+        while (r < n) {
+            char currentChar = s.charAt(r);
+
+            // Check if the current character exists in the map
+            if (mpp.containsKey(currentChar)) {
+                // Move the left pointer to avoid overlapping
+                l = Math.max(l, mpp.get(currentChar) + 1);
+                // l = mpp.get(s.charAt(r)) + 1;
             }
-            set.add(s.charAt(r));
-            maxans = Math.max(maxans, r-l+1);
+
+            // Calculate the length of the current substring
+            int len = r - l + 1;
+            maxlen = Math.max(maxlen, len);
+
+            // Update the map with the current character's index
+            mpp.put(currentChar, r);
+
+            // Move the right pointer
+            r++;
         }
-        return maxans;
+
+        return maxlen;
     }
 }
