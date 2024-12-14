9e1f32f438a9817e8d55df40f4aaabaaa3de4d3e
Smriti Eligar <smritieligar1005@gmail.com>
2024-12-14 14:07:02 +0530

    Time: 2 ms (98.82%), Space: 54.9 MB (60.95%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,27 +1,22 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
         int n = cardPoints.length;
-        int sum = 0;
-
-        for(int points : cardPoints){
-            sum = sum + points;
+        int lsum=0, rsum=0, maxsum=0;
+        int rightind = n-1;
+        for(int i = 0; i<k; i++){
+            lsum = lsum + cardPoints[i];
+            maxsum = lsum;
         }
-
-        if(k==n) return sum;
-
-        int winsize = n-k;
-        int minsubarraysum = 0;
-
-        for(int i=0;i<winsize;i++){
-            minsubarraysum = minsubarraysum + cardPoints[i];
+        for(int i = 0; i<k; i++){
+            lsum = lsum - cardPoints[k-1-i];//remove one card from right from lsum
+            rsum = rsum + cardPoints[rightind-i];//add a card to right sum
+            maxsum = Math.max(maxsum, lsum + rsum);
         }
-
-        int currentsubarraysum = minsubarraysum;
-
-        for(int i = winsize; i<n; i++){
-            currentsubarraysum = currentsubarraysum + (cardPoints[i] - cardPoints[i-winsize]);
-            minsubarraysum = Math.min(minsubarraysum,currentsubarraysum);
-        }
-        return sum - minsubarraysum;
+        return maxsum;
     }
 }
+// Initially: lsum = 1 + 2 + 3 = 6, maxsum = 6.
+// Iteration 1: lsum = 6 - 3 = 3, rsum = 1, maxsum = Math.max(6, 3 + 1) = 6.
+// Iteration 2: lsum = 3 - 2 = 1, rsum = 1 + 6 = 7, maxsum = Math.max(6, 1 + 7) = 8.
+// Iteration 3: lsum = 1 - 1 = 0, rsum = 7 + 5 = 12, maxsum = Math.max(8, 0 + 12) = 12.
+// Final answer is 12.
