88cf666c875a8251ef48443be5f5df597b3aa733
Smriti Eligar <smritieligar1005@gmail.com>
2024-12-30 16:58:31 +0530

    Time: 8 ms (80.26%), Space: 44.6 MB (62.5%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,33 +1,22 @@
 class Solution {
-    // s.charAt(r): This gets the character at index r from the string s.
-    // count.getOrDefault(key, defaultValue): This method checks if the map count
-    // has the key s.charAt(r).
-    // If s.charAt(r) is found as a key in count, getOrDefault returns the
-    // associated value.
-    // If not, it returns the specified default value 0.
-    // + 1: Adds 1 to the result of getOrDefault. This is typically used to
-    // increment the count of occurrences of s.charAt(r) in count.
     public int characterReplacement(String s, int k) {
-        if (s == null || s.length() == 0) {
-            return 0; // Handle edge case where s is null or empty
-        }
-        HashMap<Character, Integer> count = new HashMap<>();
-        int res = 0;
-        int maxf = 0;
-        int l = 0;
-        for (int r = 0; r < s.length(); r++) {
-            count.put(s.charAt(r), count.getOrDefault(s.charAt(r), 0) + 1);
-            maxf = Math.max(maxf, count.get(s.charAt(r)));
-            while ((r - l + 1) - maxf > k) {
-                count.put(s.charAt(l), count.get(s.charAt(l)) - 1);
+        int l = 0, maxlen = 0, maxfreq = 0;
+        int n = s.length();
+        int[] hash = new int[26];
+
+        for(int r = 0; r<n;r++){
+            char rc = s.charAt(r);
+            hash[rc-'A']++;
+            maxfreq = Math.max(maxfreq, hash[rc-'A']);
+            if((r-l+1)-maxfreq > k){
+                char lc = s.charAt(l);
+                hash[lc - 'A']--;
                 l++;
             }
-            res = Math.max(maxf, r - l + 1);
+            else{
+                maxlen = Math.max(maxlen, r-l+1);
+            }
         }
-        return res;
+        return maxlen;
     }
 }
-// count.put(s.charAt(l), count.get(s.charAt(l)) - 1);: This decreases the
-// frequency of the character at the left boundary (l) in the count map,
-// effectively removing it from the window.
-// l++: Moves the left boundary (l) to the right by one, shrinking the window..
